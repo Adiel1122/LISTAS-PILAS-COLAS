f36cb1b2285ccc63ad8eb595f703edeb31140764
52master <undertakeradiel@gmail.com>
2025-04-23 18:37:19 -0600

    Actualizacion Pila estatica.cpp
    Mejor manejo de nombres y funciones

--- Pila estatica.cpp
+++ Pila estatica.cpp
@@ -21,108 +21,127 @@ Da el tamaño de la pila en ese momento
 */
 
 
-
 #include <stdio.h>
 #define pf printf
+#define sf scanf
+
 
 // funciones
-  
-struct pila{  
-    int elementos[10];  
-    int tope;  
-};  
 
-void crearPila(struct pila *);  
-int pop(struct pila *);  
-int push(struct pila *, int);  
-int top(struct pila *);  
-int empty(struct pila *);  
-int full(struct pila *);  
-int size(struct pila *);  
-  
+struct pila{
+    int datos[10];
+    int tope;
+};
 
-// operacionesPila
+void crearPila(struct pila*);
+void verPila(struct pila*);
+int pop(struct pila*);
+int push(struct pila*, int);
+int top(struct pila*);
+int empty(struct pila*);
+int full(struct pila*);
+int size(struct pila*);
 
-void crearPila(struct pila *pilaNueva) {  
-    pilaNueva->tope = -1;  
-    pf("Pila creada\n");  
+
+//  Operaciones
+
+void crearPila(struct pila*pila){
+    pila->tope = -1;
+    pf("Pila creada\n");
 }
-  
 
 
-int pop(struct pila *pilaPop) {  
-    if (pilaPop->tope == -1) {  
-        pf("Pila vacia\n");  
-        return -1;  
-    } else {  
-        pf("Elemento eliminado: %d\n", pilaPop->elementos[pilaPop->tope]);  
-        pilaPop->tope--;  
-        return 0;  
-    }  
-}  
+void verPila(struct pila*pila){
+    if(pila->tope==-1){
+        pf("Pila vacia\n");
+        return;
+    }
+    pf("Datos de la pila\n");
+    for(int i=top(pila)-1; i>=0; i--){
+        pf("%d\n",pila->datos[i]);
+    }
+}
 
 
-
-int push(struct pila *pilaPush, int nuevoDato) {  
-    if (pilaPush->tope == 9) {  
-        pf("Pila llena\n");  
-        return -1;  
-    } else {  
-        pilaPush->tope++;  
-        pilaPush->elementos[pilaPush->tope] = nuevoDato;  
-        pf("Dato ingresado correctamente: %d\n", nuevoDato);  
-        return 0;  
-    }  
-}  
-
+int push(struct pila*pila, int nuevoDato){
+    if(pila->tope==9){
+        pf("Pila llena\n");
+        return -1;
+    } else{
+        pila->tope++;
+        pila->datos[pila->tope] = nuevoDato;
+        pf("Dato ingresado: %d\n", nuevoDato);
+        return 0;
+    }
+}
 
 
-int top(struct pila *pilaTop) {  
-    if (pilaTop->tope == -1) {  
-        pf("Pila vacia, no hay elemento en la cima\n");  
-        return -1;  
-    } else {  
-        return pilaTop->elementos[pilaTop->tope];  
-    }  
-}  
+int pop(struct pila*pila){
+    if(pila->tope==-1){
+        pf("Pila vacia\n");
+        return -1;
+    } else{
+        pf("Elemento eliminado: %d\n",pila->datos[pila->tope]);
+        pila->tope--;
+        return 0;
+    }
+}
 
 
-
-int empty(struct pila *pilaChecar) {  
-    return pilaChecar->tope == -1;  
-}  
-
-
-
-int full(struct pila *pilaChecar) {  
-    return pilaChecar->tope == 9;  
-}  
+int top(struct pila*pila){
+    if(pila->tope==-1){
+        pf("Pila vacia\n");
+        return -1;
+    }
+    return pila->datos[pila->tope];
+}
 
 
+int empty(struct pila*pila){
+    if(pila->tope!=-1){
+        pf("Pila con elementos\n");
+        return -1;
+    }
+    pf("Pila vacia\n");
+    return 0;
+}
 
-int size(struct pila *pilaSize) {  
-    return pilaSize->tope + 1; // +1 porque tope comienza en -1  
-}  
+
+int full(struct pila* pila){
+    if(pila->tope!=9){
+        pf("Pila no llena\n");
+        return -1;
+    }
+    pf("Pila llena\n");
+    return 0;
+}
 
 
+int size(struct pila* pila){
+    return pila->tope+1;
+}
 
-// Main
 
-int main() {  
-    struct pila miPila;  
-    crearPila(&miPila);  
-    
-    push(&miPila, 100);  
-    push(&miPila, 50);  
-    
-    pf("Elemento en la cima: %d\n", top(&miPila));  
+// main
 
-    pop(&miPila);  
-    pf("¿Pila vacia? %s\n", empty(&miPila) ? "Si" : "No");  
-    pf("Tamaño de la pila: %d\n", size(&miPila));  
-    
-    pop(&miPila);  
-    pf("¿Pila vacia? %s\n", empty(&miPila) ? "Si" : "No");  
+int main(){
+    struct pila miPila;
+    crearPila(&miPila);
 
-    return 0;  
+    push(&miPila, 1);
+    push(&miPila, 2);
+    push(&miPila, 3);
+    push(&miPila, 4);
+    verPila(&miPila);
+
+    pop(&miPila);
+    pop(&miPila);
+
+    verPila(&miPila);
+
+    empty(&miPila);
+    full(&miPila);
+    pf("Tamaño de la pila: %d\n",size(&miPila));
+
+    return 0;
 }
